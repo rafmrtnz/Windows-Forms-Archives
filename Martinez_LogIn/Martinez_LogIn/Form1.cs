382bb5c69716ad304cdc553aee3c56cd6a075b34
raf <raffimartin069@gmail.com>
2024-10-18 00:37:12 +0800

    added new activities

---
+++ Martinez_LogIn/Martinez_LogIn/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Martinez_LogIn
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			var url = (NameValueCollection)ConfigurationManager.GetSection("appSettings");
+			Process.Start(url["URL"]);
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+				textBox1.UseSystemPasswordChar = false;
+			else
+				textBox1.UseSystemPasswordChar = true;
+		}
+	}
+}
---
+++ Martinez_ResponsiveUI/Martinez_ResponsiveUI/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Martinez_ResponsiveUI
+{
+	partial class myForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.AuthenticationForm = new System.Windows.Forms.FlowLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.GoogleSignIn = new System.Windows.Forms.Button();
+			this.FacebookSignIn = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.AuthenticationForm.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.tableLayoutPanel4.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 639F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 283F));
+			this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.AuthenticationForm, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1264, 681);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// AuthenticationForm
+			// 
+			this.AuthenticationForm.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.AuthenticationForm.AutoSize = true;
+			this.AuthenticationForm.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.AuthenticationForm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.AuthenticationForm.Controls.Add(this.tableLayoutPanel2);
+			this.AuthenticationForm.Controls.Add(this.label1);
+			this.AuthenticationForm.Controls.Add(this.textBox2);
+			this.AuthenticationForm.Controls.Add(this.label2);
+			this.AuthenticationForm.Controls.Add(this.textBox1);
+			this.AuthenticationForm.Controls.Add(this.button1);
+			this.AuthenticationForm.Controls.Add(this.tableLayoutPanel4);
+			this.AuthenticationForm.Controls.Add(this.label4);
+			this.AuthenticationForm.Controls.Add(this.tableLayoutPanel3);
+			this.AuthenticationForm.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.AuthenticationForm.Location = new System.Drawing.Point(101, 42);
+			this.AuthenticationForm.Margin = new System.Windows.Forms.Padding(0);
+			this.AuthenticationForm.Name = "AuthenticationForm";
+			this.AuthenticationForm.Padding = new System.Windows.Forms.Padding(50);
+			this.AuthenticationForm.Size = new System.Drawing.Size(437, 596);
+			this.AuthenticationForm.TabIndex = 3;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+			this.tableLayoutPanel2.ForeColor = System.Drawing.Color.White;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(53, 53);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(331, 101);
+			this.tableLayoutPanel2.TabIndex = 6;
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 28F, System.Drawing.FontStyle.Bold);
+			this.label3.Location = new System.Drawing.Point(3, 0);
+			this.label3.Name = "label3";
+			this.label3.Padding = new System.Windows.Forms.Padding(0, 0, 0, 50);
+			this.label3.Size = new System.Drawing.Size(325, 101);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Kape de Cebuano";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(53, 157);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 21);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Username";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(53, 181);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(331, 26);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(53, 227);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(79, 21);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Password";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(53, 251);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(331, 26);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.UseSystemPasswordChar = true;
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(96)))), ((int)(((byte)(59)))));
+			this.button1.Location = new System.Drawing.Point(60, 297);
+			this.button1.Margin = new System.Windows.Forms.Padding(10);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(317, 31);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Sign In";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// tableLayoutPanel4
+			// 
+			this.tableLayoutPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel4.AutoSize = true;
+			this.tableLayoutPanel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel4.ColumnCount = 2;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel4.Controls.Add(this.label6, 1, 0);
+			this.tableLayoutPanel4.Controls.Add(this.label5, 0, 0);
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(53, 341);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.Padding = new System.Windows.Forms.Padding(0, 25, 0, 50);
+			this.tableLayoutPanel4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.tableLayoutPanel4.RowCount = 1;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(331, 90);
+			this.tableLayoutPanel4.TabIndex = 9;
+			// 
+			// label6
+			// 
+			this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label6.AutoSize = true;
+			this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(168, 25);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(160, 15);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "Forgot Password";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label5.AutoSize = true;
+			this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(3, 25);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(159, 15);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "Sign Up";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Pixel);
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(53, 434);
+			this.label4.Margin = new System.Windows.Forms.Padding(3, 0, 3, 25);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(331, 15);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "--- or using ---";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.AutoSize = true;
+			this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.Controls.Add(this.GoogleSignIn, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.FacebookSignIn, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.AuthenticationForm.SetFlowBreak(this.tableLayoutPanel3, true);
+			this.tableLayoutPanel3.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(53, 477);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(331, 66);
+			this.tableLayoutPanel3.TabIndex = 7;
+			// 
+			// GoogleSignIn
+			// 
+			this.GoogleSignIn.BackgroundImage = global::Martinez_ResponsiveUI.Properties.Resources.icons8_google_50;
+			this.GoogleSignIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.GoogleSignIn.Dock = System.Windows.Forms.DockStyle.Left;
+			this.GoogleSignIn.Location = new System.Drawing.Point(3, 13);
+			this.GoogleSignIn.MinimumSize = new System.Drawing.Size(50, 50);
+			this.GoogleSignIn.Name = "GoogleSignIn";
+			this.GoogleSignIn.Size = new System.Drawing.Size(158, 50);
+			this.GoogleSignIn.TabIndex = 1;
+			this.GoogleSignIn.UseVisualStyleBackColor = true;
+			// 
+			// FacebookSignIn
+			// 
+			this.FacebookSignIn.BackgroundImage = global::Martinez_ResponsiveUI.Properties.Resources.icons8_facebook_48;
+			this.FacebookSignIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.FacebookSignIn.Dock = System.Windows.Forms.DockStyle.Left;
+			this.FacebookSignIn.Location = new System.Drawing.Point(167, 13);
+			this.FacebookSignIn.MinimumSize = new System.Drawing.Size(50, 50);
+			this.FacebookSignIn.Name = "FacebookSignIn";
+			this.FacebookSignIn.Size = new System.Drawing.Size(158, 50);
+			this.FacebookSignIn.TabIndex = 2;
+			this.FacebookSignIn.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Image = global::Martinez_ResponsiveUI.Properties.Resources.jon_tyson_uJ_GU9hwWgU_unsplash;
+			this.pictureBox1.Location = new System.Drawing.Point(642, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(619, 675);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// myForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(60)))), ((int)(((byte)(52)))));
+			this.ClientSize = new System.Drawing.Size(1264, 681);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			this.Name = "myForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.AuthenticationForm.ResumeLayout(false);
+			this.AuthenticationForm.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.tableLayoutPanel4.ResumeLayout(false);
+			this.tableLayoutPanel4.PerformLayout();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.FlowLayoutPanel AuthenticationForm;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Button GoogleSignIn;
+		private System.Windows.Forms.Button FacebookSignIn;
+	}
+}
+
